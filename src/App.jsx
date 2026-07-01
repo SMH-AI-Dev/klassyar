c9bc3550c3b20c6bcd3061416054a859371734ef
SMH AI Dev <smh-ai-dev@github.com>
2026-07-01 04:50:16 +0330

    perf: code-splitting with React.lazy + manualChunks, no more chunk size warning

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,103 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense, lazy } from 'react';
 import { AuthProvider, useAuth } from './context/AuthContext';
 import ErrorBoundary from './components/shared/ErrorBoundary';
 import { BrowserRouter, Routes, Route, Navigate, useSearchParams } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastProvider, useToast } from './components/ui/toast';
 import Layout from './components/layout/Layout';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import SavedActivities from './pages/SavedActivities';
-import Features from './pages/Features';
-import About from './pages/About';
-import MyClasses from './pages/MyClasses';
-import Login from './pages/Login';
-import TeacherReports from './pages/TeacherReports';
-import LanguageLearning from './pages/LanguageLearning';
-import GradeSelection from './pages/GradeSelection';
-import LearningOptions from './pages/LearningOptions';
-import LearningItems from './pages/LearningItems';
-import GameHub from './pages/GameHub';
-import EnglishGameHub from './pages/EnglishGameHub';
-import PlayGame from './pages/PlayGame';
-import PlayEnglish from './pages/PlayEnglish';
 
-// Create Pages
-import CreateFlashcard from './pages/CreateFlashcard';
-import CreateQuiz from './pages/CreateQuiz';
-import CreateMatching from './pages/CreateMatching';
-import CreateGroup_sort from './pages/CreateGroup_sort';
-import CreateMemory_game from './pages/CreateMemory_game';
-import CreateRandom_wheel from './pages/CreateRandom_wheel';
-import CreateTrue_false from './pages/CreateTrue_false';
-import CreateUnjumble from './pages/CreateUnjumble';
-import CreateWhack_a_mole from './pages/CreateWhack_a_mole';
-import CreateWordsearch from './pages/CreateWordsearch';
-import CreateFill_blank from './pages/CreateFill_blank';
-import CreateRanking from './pages/CreateRanking';
-import CreateCrossword from './pages/CreateCrossword';
-import CreateTimeline from './pages/CreateTimeline';
-import CreateSpinner from './pages/CreateSpinner';
-import CreateCard_flip from './pages/CreateCard_flip';
-import CreateHotspot from './pages/CreateHotspot';
-import CreateDrag_text from './pages/CreateDrag_text';
-import CreateBalloon_pop from './pages/CreateBalloon_pop';
-import CreateGameshow_quiz from './pages/CreateGameshow_quiz';
-import CreateMaze_chase from './pages/CreateMaze_chase';
-import CreateAirplane from './pages/CreateAirplane';
-import CreateOpen_the_box from './pages/CreateOpen_the_box';
-import CreateMissing_word from './pages/CreateMissing_word';
-import CreateAnagram from './pages/CreateAnagram';
-import CreateHangman from './pages/CreateHangman';
-import CreateFlip_tiles from './pages/CreateFlip_tiles';
-import CreateImage_quiz from './pages/CreateImage_quiz';
-import CreateLabelled_diagram from './pages/CreateLabelled_diagram';
-import CreateRandom_cards from './pages/CreateRandom_cards';
+const Home = lazy(() => import('./pages/Home'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const SavedActivities = lazy(() => import('./pages/SavedActivities'));
+const Features = lazy(() => import('./pages/Features'));
+const About = lazy(() => import('./pages/About'));
+const MyClasses = lazy(() => import('./pages/MyClasses'));
+const Login = lazy(() => import('./pages/Login'));
+const TeacherReports = lazy(() => import('./pages/TeacherReports'));
+const LanguageLearning = lazy(() => import('./pages/LanguageLearning'));
+const GradeSelection = lazy(() => import('./pages/GradeSelection'));
+const LearningOptions = lazy(() => import('./pages/LearningOptions'));
+const LearningItems = lazy(() => import('./pages/LearningItems'));
+const GameHub = lazy(() => import('./pages/GameHub'));
+const EnglishGameHub = lazy(() => import('./pages/EnglishGameHub'));
+const PlayGame = lazy(() => import('./pages/PlayGame'));
+const PlayEnglish = lazy(() => import('./pages/PlayEnglish'));
 
-// Play Pages
-import PlayQuiz from './pages/PlayQuiz';
-import PlayFlashcard from './pages/PlayFlashcard';
-import PlayRandom_wheel from './pages/PlayRandom_wheel';
-import PlayTrue_false from './pages/PlayTrue_false';
-import PlayUnjumble from './pages/PlayUnjumble';
-import PlayMatching from './pages/PlayMatching';
-import PlayMemory_game from './pages/PlayMemory_game';
-import PlayGroup_sort from './pages/PlayGroup_sort';
-import PlayWordsearch from './pages/PlayWordsearch';
-import PlayWhack_a_mole from './pages/PlayWhack_a_mole';
-import PlayFill_blank from './pages/PlayFill_blank';
-import PlayRanking from './pages/PlayRanking';
-import PlaySpinner from './pages/PlaySpinner';
-import PlayTimeline from './pages/PlayTimeline';
-import PlayCrossword from './pages/PlayCrossword';
-import PlayCard_flip from './pages/PlayCard_flip';
-import PlayDrag_text from './pages/PlayDrag_text';
-import PlayBalloon_pop from './pages/PlayBalloon_pop';
-import PlayMissing_word from './pages/PlayMissing_word';
-import PlayAnagram from './pages/PlayAnagram';
-import PlayRandom_cards from './pages/PlayRandom_cards';
-import PlayAirplane from './pages/PlayAirplane';
-import PlayGameshow_quiz from './pages/PlayGameshow_quiz';
-import PlayHangman from './pages/PlayHangman';
-import PlayHotspot from './pages/PlayHotspot';
-import PlayImage_quiz from './pages/PlayImage_quiz';
-import PlayLabelled_diagram from './pages/PlayLabelled_diagram';
-import PlayMaze_chase from './pages/PlayMaze_chase';
-import PlayOpen_the_box from './pages/PlayOpen_the_box';
-import PlayFlip_tiles from './pages/PlayFlip_tiles';
+const CreateFlashcard = lazy(() => import('./pages/CreateFlashcard'));
+const CreateQuiz = lazy(() => import('./pages/CreateQuiz'));
+const CreateMatching = lazy(() => import('./pages/CreateMatching'));
+const CreateGroup_sort = lazy(() => import('./pages/CreateGroup_sort'));
+const CreateMemory_game = lazy(() => import('./pages/CreateMemory_game'));
+const CreateRandom_wheel = lazy(() => import('./pages/CreateRandom_wheel'));
+const CreateTrue_false = lazy(() => import('./pages/CreateTrue_false'));
+const CreateUnjumble = lazy(() => import('./pages/CreateUnjumble'));
+const CreateWhack_a_mole = lazy(() => import('./pages/CreateWhack_a_mole'));
+const CreateWordsearch = lazy(() => import('./pages/CreateWordsearch'));
+const CreateFill_blank = lazy(() => import('./pages/CreateFill_blank'));
+const CreateRanking = lazy(() => import('./pages/CreateRanking'));
+const CreateCrossword = lazy(() => import('./pages/CreateCrossword'));
+const CreateTimeline = lazy(() => import('./pages/CreateTimeline'));
+const CreateSpinner = lazy(() => import('./pages/CreateSpinner'));
+const CreateCard_flip = lazy(() => import('./pages/CreateCard_flip'));
+const CreateHotspot = lazy(() => import('./pages/CreateHotspot'));
+const CreateDrag_text = lazy(() => import('./pages/CreateDrag_text'));
+const CreateBalloon_pop = lazy(() => import('./pages/CreateBalloon_pop'));
+const CreateGameshow_quiz = lazy(() => import('./pages/CreateGameshow_quiz'));
+const CreateMaze_chase = lazy(() => import('./pages/CreateMaze_chase'));
+const CreateAirplane = lazy(() => import('./pages/CreateAirplane'));
+const CreateOpen_the_box = lazy(() => import('./pages/CreateOpen_the_box'));
+const CreateMissing_word = lazy(() => import('./pages/CreateMissing_word'));
+const CreateAnagram = lazy(() => import('./pages/CreateAnagram'));
+const CreateHangman = lazy(() => import('./pages/CreateHangman'));
+const CreateFlip_tiles = lazy(() => import('./pages/CreateFlip_tiles'));
+const CreateImage_quiz = lazy(() => import('./pages/CreateImage_quiz'));
+const CreateLabelled_diagram = lazy(() => import('./pages/CreateLabelled_diagram'));
+const CreateRandom_cards = lazy(() => import('./pages/CreateRandom_cards'));
 
-// Edit Pages
-import EditQuiz from './pages/EditQuiz';
-import EditFlashcard from './pages/EditFlashcard';
-import EditMatching from './pages/EditMatching';
-import EditMemory_game from './pages/EditMemory_game';
-import EditGroup_sort from './pages/EditGroup_sort';
-import EditWordsearch from './pages/EditWordsearch';
-import EditRandom_wheel from './pages/EditRandom_wheel';
-import EditUnjumble from './pages/EditUnjumble';
-import EditTrue_false from './pages/EditTrue_false';
-import EditWhack_a_mole from './pages/EditWhack_a_mole';
-import EditFill_blank from './pages/EditFill_blank';
-import EditRanking from './pages/EditRanking';
-import EditSpinner from './pages/EditSpinner';
+const PlayQuiz = lazy(() => import('./pages/PlayQuiz'));
+const PlayFlashcard = lazy(() => import('./pages/PlayFlashcard'));
+const PlayRandom_wheel = lazy(() => import('./pages/PlayRandom_wheel'));
+const PlayTrue_false = lazy(() => import('./pages/PlayTrue_false'));
+const PlayUnjumble = lazy(() => import('./pages/PlayUnjumble'));
+const PlayMatching = lazy(() => import('./pages/PlayMatching'));
+const PlayMemory_game = lazy(() => import('./pages/PlayMemory_game'));
+const PlayGroup_sort = lazy(() => import('./pages/PlayGroup_sort'));
+const PlayWordsearch = lazy(() => import('./pages/PlayWordsearch'));
+const PlayWhack_a_mole = lazy(() => import('./pages/PlayWhack_a_mole'));
+const PlayFill_blank = lazy(() => import('./pages/PlayFill_blank'));
+const PlayRanking = lazy(() => import('./pages/PlayRanking'));
+const PlaySpinner = lazy(() => import('./pages/PlaySpinner'));
+const PlayTimeline = lazy(() => import('./pages/PlayTimeline'));
+const PlayCrossword = lazy(() => import('./pages/PlayCrossword'));
+const PlayCard_flip = lazy(() => import('./pages/PlayCard_flip'));
+const PlayDrag_text = lazy(() => import('./pages/PlayDrag_text'));
+const PlayBalloon_pop = lazy(() => import('./pages/PlayBalloon_pop'));
+const PlayMissing_word = lazy(() => import('./pages/PlayMissing_word'));
+const PlayAnagram = lazy(() => import('./pages/PlayAnagram'));
+const PlayRandom_cards = lazy(() => import('./pages/PlayRandom_cards'));
+const PlayAirplane = lazy(() => import('./pages/PlayAirplane'));
+const PlayGameshow_quiz = lazy(() => import('./pages/PlayGameshow_quiz'));
+const PlayHangman = lazy(() => import('./pages/PlayHangman'));
+const PlayHotspot = lazy(() => import('./pages/PlayHotspot'));
+const PlayImage_quiz = lazy(() => import('./pages/PlayImage_quiz'));
+const PlayLabelled_diagram = lazy(() => import('./pages/PlayLabelled_diagram'));
+const PlayMaze_chase = lazy(() => import('./pages/PlayMaze_chase'));
+const PlayOpen_the_box = lazy(() => import('./pages/PlayOpen_the_box'));
+const PlayFlip_tiles = lazy(() => import('./pages/PlayFlip_tiles'));
+
+const EditQuiz = lazy(() => import('./pages/EditQuiz'));
+const EditFlashcard = lazy(() => import('./pages/EditFlashcard'));
+const EditMatching = lazy(() => import('./pages/EditMatching'));
+const EditMemory_game = lazy(() => import('./pages/EditMemory_game'));
+const EditGroup_sort = lazy(() => import('./pages/EditGroup_sort'));
+const EditWordsearch = lazy(() => import('./pages/EditWordsearch'));
+const EditRandom_wheel = lazy(() => import('./pages/EditRandom_wheel'));
+const EditUnjumble = lazy(() => import('./pages/EditUnjumble'));
+const EditTrue_false = lazy(() => import('./pages/EditTrue_false'));
+const EditWhack_a_mole = lazy(() => import('./pages/EditWhack_a_mole'));
+const EditFill_blank = lazy(() => import('./pages/EditFill_blank'));
+const EditRanking = lazy(() => import('./pages/EditRanking'));
+const EditSpinner = lazy(() => import('./pages/EditSpinner'));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -111,9 +109,20 @@ const queryClient = new QueryClient({
   },
 });
 
+function PageLoader() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-900 to-pink-900">
+      <div className="text-center">
+        <div className="w-12 h-12 border-4 border-purple-400 border-t-transparent rounded-full animate-spin mx-auto mb-4" />
+        <p className="text-purple-200 text-sm">کلاس یار...</p>
+      </div>
+    </div>
+  );
+}
+
 function IndexRouter() {
   const { isLoggedIn, loading } = useAuth();
-  if (loading) return <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-900 to-pink-900"><div className="w-10 h-10 border-4 border-purple-400 border-t-transparent rounded-full animate-spin" /></div>;
+  if (loading) return <PageLoader />;
   if (!isLoggedIn) return <Login />;
   return <Layout><Home /></Layout>;
 }
@@ -131,39 +140,37 @@ function AppContent() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Public routes (no Layout) */}
-        <Route path="/Login" element={<Login />} />
-        
-        {/* App routes (with Layout) */}
-        <Route element={<PublicRoute />}>
-          <Route path="/" element={<IndexRouter />} />
-        </Route>
-        <Route path="/Home" element={<Layout><Home /></Layout>} />
-        <Route path="/Dashboard" element={<Layout><Dashboard /></Layout>} />
-        <Route path="/SavedActivities" element={<Layout><SavedActivities /></Layout>} />
-        <Route path="/Features" element={<Layout><Features /></Layout>} />
-        <Route path="/About" element={<Layout><About /></Layout>} />
-        <Route path="/MyClasses" element={<Layout><MyClasses /></Layout>} />
-        <Route path="/TeacherReports" element={<Layout><TeacherReports /></Layout>} />
-        <Route path="/LanguageLearning" element={<Layout><LanguageLearning /></Layout>} />
-        <Route path="/LanguageLearning/grades" element={<Layout><GradeSelection /></Layout>} />
-        <Route path="/LanguageLearning/:gradeId" element={<Layout><LearningOptions /></Layout>} />
-        <Route path="/LanguageLearning/:gradeId/:optionId" element={<Layout><LearningItems /></Layout>} />
-        <Route path="/game-hub" element={<Layout><GameHub /></Layout>} />
-        <Route path="/english-hub" element={<Layout><EnglishGameHub /></Layout>} />
-        <Route path="/play-game/:gameId" element={<PlayGame />} />
-        <Route path="/play-english/:gameId" element={<PlayEnglish />} />
-        
-        {/* Create Route */}
-        <Route path="/Create" element={<CreateRouter />} />
-        
-        {/* Play & Edit Routes */}
-        <Route path="/Play" element={<PlayRouter />} />
-        <Route path="/Edit" element={<EditRouter />} />
-        
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <Suspense fallback={<PageLoader />}>
+        <Routes>
+          <Route path="/Login" element={<Login />} />
+          
+          <Route element={<PublicRoute />}>
+            <Route path="/" element={<IndexRouter />} />
+          </Route>
+          <Route path="/Home" element={<Layout><Home /></Layout>} />
+          <Route path="/Dashboard" element={<Layout><Dashboard /></Layout>} />
+          <Route path="/SavedActivities" element={<Layout><SavedActivities /></Layout>} />
+          <Route path="/Features" element={<Layout><Features /></Layout>} />
+          <Route path="/About" element={<Layout><About /></Layout>} />
+          <Route path="/MyClasses" element={<Layout><MyClasses /></Layout>} />
+          <Route path="/TeacherReports" element={<Layout><TeacherReports /></Layout>} />
+          <Route path="/LanguageLearning" element={<Layout><LanguageLearning /></Layout>} />
+          <Route path="/LanguageLearning/grades" element={<Layout><GradeSelection /></Layout>} />
+          <Route path="/LanguageLearning/:gradeId" element={<Layout><LearningOptions /></Layout>} />
+          <Route path="/LanguageLearning/:gradeId/:optionId" element={<Layout><LearningItems /></Layout>} />
+          <Route path="/game-hub" element={<Layout><GameHub /></Layout>} />
+          <Route path="/english-hub" element={<Layout><EnglishGameHub /></Layout>} />
+          <Route path="/play-game/:gameId" element={<PlayGame />} />
+          <Route path="/play-english/:gameId" element={<PlayEnglish />} />
+          
+          <Route path="/Create" element={<CreateRouter />} />
+          
+          <Route path="/Play" element={<PlayRouter />} />
+          <Route path="/Edit" element={<EditRouter />} />
+          
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
@@ -182,7 +189,6 @@ function App() {
   );
 }
 
-// Router component to handle different create types based on activity type
 function CreateRouter() {
   const [searchParams] = useSearchParams();
   const type = searchParams.get('type') || 'quiz';
@@ -224,7 +230,6 @@ function CreateRouter() {
   return <Component />;
 }
 
-// Router component to handle different activity types based on type param
 function PlayRouter() {
   const [searchParams] = useSearchParams();
   const type = searchParams.get('type') || 'quiz';
@@ -266,7 +271,6 @@ function PlayRouter() {
   return <Component />;
 }
 
-// Router component to handle different edit types based on activity type
 function EditRouter() {
   const [searchParams] = useSearchParams();
   const type = searchParams.get('type') || 'quiz';
